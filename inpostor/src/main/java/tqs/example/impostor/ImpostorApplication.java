1044396f4daa68ac94219842f3e7882b82094618
Maciej Adamczewski <macadamczewski@gmail.com>
2023-05-19 00:27:25 +0100

    InPostOr initial Spring Boot project.
    Upload of the initialization of InPostOr Spring Boot backend.

---
+++ inpostor/src/main/java/tqs/example/impostor/ImpostorApplication.java
@@ -0,0 +1,13 @@
+package tqs.example.impostor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImpostorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImpostorApplication.class, args);
+	}
+
+}
---
+++ inpostor/src/test/java/tqs/example/impostor/ImpostorApplicationTests.java
@@ -0,0 +1,13 @@
+package tqs.example.impostor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImpostorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
