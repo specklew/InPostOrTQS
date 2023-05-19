f4df1624d37b74fd21d2bef3361bf410206f20d2
Maciej Adamczewski <macadamczewski@gmail.com>
2023-05-19 12:08:46 +0100

    ACP repository interface and ACP entity.
    For now the ACP only contains info about the address and capacity. It may have to be expanded in the future, but I am leaving it as it is for now.

---
+++ inpostor/src/main/java/tqs/example/impostor/repository/ACPRepository.java
@@ -0,0 +1,11 @@
+package tqs.example.impostor.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface ACPRepository extends JpaRepository<ACP, Long> {
+
+    boolean existsByAddress(String address);
+    Optional<ACP> findByAddress(String address);
+}
