afb5b86f656442fc76857efbf80f8a96cf157106
Maciej Adamczewski <macadamczewski@gmail.com>
2023-05-25 12:39:19 +0100

    Minor OrderRepository refactor.

--- src/test/java/tqs/example/impostor/repository/OrderRepositoryTest.java
+++ src/test/java/tqs/example/impostor/repository/OrderRepositoryTest.java
@@ -13,7 +13,7 @@ import java.util.Optional;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest
-public class OrderRepositoryTest {
+class OrderRepositoryTest {
 
     @Autowired
     private OrderRepository orderRepository;
