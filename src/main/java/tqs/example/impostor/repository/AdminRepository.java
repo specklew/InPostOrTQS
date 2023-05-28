12f944a8f59606c4461f1c34851d32d730949f29
Juliusz Szymajda <234769@edu.p.lodz.pl>
2023-05-28 17:57:11 +0100

    AdminRepository filled.

--- src/main/java/tqs/example/impostor/repository/AdminRepository.java
+++ src/main/java/tqs/example/impostor/repository/AdminRepository.java
@@ -1,4 +1,13 @@
 package tqs.example.impostor.repository;
 
-public interface AdminRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface AdminRepository extends JpaRepository<Admin, Long>{
+
+    Optional<Admin> findById(Long id);
+    Optional<Admin> findByUserName(String userName);
 }
