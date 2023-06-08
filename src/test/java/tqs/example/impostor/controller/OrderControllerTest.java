d22f92c86a012cbcfb3cc0704653e4070830b07a
Maciej Adamczewski <macadamczewski@gmail.com>
2023-06-08 13:53:22 +0100

    All Order read, update and delete tests.

--- src/test/java/tqs/example/impostor/controller/OrderControllerTest.java
+++ src/test/java/tqs/example/impostor/controller/OrderControllerTest.java
@@ -170,4 +170,119 @@ class OrderControllerTest {
                 .andExpect(jsonPath("$[*].deliverer",
                         containsInAnyOrder(orderList.stream().map(Order::getDeliverer).toArray())));
     }
+
+    @ParameterizedTest
+    @ValueSource(ints = {0,1,2})
+    void givenOwner_whenGetOrdersByOwner_thenReturnListOfOrders(int id) throws Exception {
+
+        List<Order> orderList = new ArrayList<>();
+
+        for(Order order : orders){
+            if(Objects.equals(order.getOwner(), orders.get(id).getOwner())){
+                orderList.add(order);
+            }
+        }
+
+        when(service.readOrdersByOwner(orders.get(id).getOwner())).thenReturn(orderList);
+
+        mvc.perform(get("/order/getByOwner/" + orders.get(id).getOwner()))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[*].id",
+                        containsInAnyOrder(orderList.stream().map(order -> order.getId().intValue()).toArray())))
+                .andExpect(jsonPath("$[*].shopName",
+                        containsInAnyOrder(orderList.stream().map(Order::getShopName).toArray())))
+                .andExpect(jsonPath("$[*].acp.address",
+                        containsInAnyOrder(orderList.stream().map(order -> order.getAcp().getAddress()).toArray())))
+                .andExpect(jsonPath("$[*].owner",
+                        containsInAnyOrder(orderList.stream().map(Order::getOwner).toArray())))
+                .andExpect(jsonPath("$[*].deliverer",
+                        containsInAnyOrder(orderList.stream().map(Order::getDeliverer).toArray())));
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {0,1,2})
+    void givenShopName_whenGetOrdersByShopName_thenReturnListOfOrders(int id) throws Exception {
+
+        List<Order> orderList = new ArrayList<>();
+
+        for(Order order : orders){
+            if(Objects.equals(order.getShopName(), orders.get(id).getShopName())){
+                orderList.add(order);
+            }
+        }
+
+        when(service.readOrdersByShopName(orders.get(id).getShopName())).thenReturn(orderList);
+
+        mvc.perform(get("/order/getByShopName/" + orders.get(id).getShopName()))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[*].id",
+                        containsInAnyOrder(orderList.stream().map(order -> order.getId().intValue()).toArray())))
+                .andExpect(jsonPath("$[*].shopName",
+                        containsInAnyOrder(orderList.stream().map(Order::getShopName).toArray())))
+                .andExpect(jsonPath("$[*].acp.address",
+                        containsInAnyOrder(orderList.stream().map(order -> order.getAcp().getAddress()).toArray())))
+                .andExpect(jsonPath("$[*].owner",
+                        containsInAnyOrder(orderList.stream().map(Order::getOwner).toArray())))
+                .andExpect(jsonPath("$[*].deliverer",
+                        containsInAnyOrder(orderList.stream().map(Order::getDeliverer).toArray())));
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {0,1,2})
+    void givenDeliverer_whenGetOrdersByDeliverer_thenReturnListOfOrders(int id) throws Exception {
+
+        List<Order> orderList = new ArrayList<>();
+
+        for(Order order : orders){
+            if(Objects.equals(order.getDeliverer(), orders.get(id).getDeliverer())){
+                orderList.add(order);
+            }
+        }
+
+        when(service.readOrdersByDeliverer(orders.get(id).getDeliverer())).thenReturn(orderList);
+
+        mvc.perform(get("/order/getByDeliverer/" + orders.get(id).getDeliverer()))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[*].id",
+                        containsInAnyOrder(orderList.stream().map(order -> order.getId().intValue()).toArray())))
+                .andExpect(jsonPath("$[*].shopName",
+                        containsInAnyOrder(orderList.stream().map(Order::getShopName).toArray())))
+                .andExpect(jsonPath("$[*].acp.address",
+                        containsInAnyOrder(orderList.stream().map(order -> order.getAcp().getAddress()).toArray())))
+                .andExpect(jsonPath("$[*].owner",
+                        containsInAnyOrder(orderList.stream().map(Order::getOwner).toArray())))
+                .andExpect(jsonPath("$[*].deliverer",
+                        containsInAnyOrder(orderList.stream().map(Order::getDeliverer).toArray())));
+    }
+
+    @Test
+    void givenParameters_whenPostUpdateOrder_thenUpdateOrder() throws Exception {
+        when(service.updateOrder(Mockito.any(Long.class), Mockito.any(), Mockito.any(), Mockito.any(), Mockito.any())).thenReturn(true);
+
+        MvcResult result = mvc.perform(
+                        post("/order/update")
+                                .param("id", "1")
+                                .param("acpAddress", "test")
+                                .param("shopName", "name")
+                                .param("owner", "own")
+                                .param("deliverer", "deli"))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        String content = result.getResponse().getContentAsString();
+        assertThat(content).isEqualTo("true");
+    }
+
+    @Test
+    void givenPathVar_whenPostDeleteOrder_thenDeleteOrder() throws Exception {
+        when(service.deleteOrder(Mockito.any(Long.class))).thenReturn(true);
+
+        MvcResult result = mvc.perform(
+                        post("/order/delete/1"))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        String content = result.getResponse().getContentAsString();
+        assertThat(content).isEqualTo("true");
+    }
 }
